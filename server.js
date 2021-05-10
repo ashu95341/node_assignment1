320ce794410d58ecf56ed534ea1c0caa04ace8d9
ashu95341 <75393814+ashu95341@users.noreply.github.com>
2021-05-10 12:30:52 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const http = require('http');
 
 const fs = require('fs');
 
+const apiData = fs.readFileSync("data.json",'utf-8')
+let Data = JSON.parse(apiData);
+
 const server = http.createServer((req, res) => {
-    const pathname = req.url
+    const pathname = req.url;
+    let data = [];
 
     if(pathname === "/home"){
-        res.writeHead(200)
+        res.writeHead(200);
         res.end("welcome to home");
     }
     if(pathname === "/movie"){
@@ -16,14 +20,29 @@ const server = http.createServer((req, res) => {
         res.end("welcome to cricket");
     }
     if(pathname === "/update"){
-        res.end("welcome to update");
+        req.on("data", (line)=>{
+            data.push(String(line))
+        })
+        req.on("end",()=>{
+            data = JSON.parse(data);
+            // console.log(data)
+            Data.push(data)
+
+            let write = JSON.stringify(Data)
+
+            fs.writeFile("data.json", write, (err)=>{
+                console.log(err)
+            })
+        })
+        res.end("data uploaded successfully")
     }
     
     
     else{
-        res.end("error");
+        res.writeHead(404)
+        res.end("page not found");
     }
 
-})
+});
 
 server.listen(3000)
